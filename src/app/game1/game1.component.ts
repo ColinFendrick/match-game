cd7a9941e969f7d9c7d8ca53071350ea10b3f3bf
Colin Fendrick <colin.fendrick@gmail.com>
2020-02-24 06:06:46 -0500

    :sparkles: Add the game components, style, set everything up

---
+++ src/app/game1/game1.component.ts
@@ -0,0 +1,13 @@
+import { Component, OnInit } from '@angular/core';
+import { ANIMALS } from '../animals';
+@Component({
+  selector: 'app-game1',
+  templateUrl: './game1.component.html',
+  styleUrls: ['./game1.component.css']
+})
+export class Game1Component implements OnInit {
+  public animals = ANIMALS;
+  constructor() { }
+  ngOnInit() {
+  }
+}
---
+++ src/app/matching-game/matching-game.component.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit, Input } from '@angular/core';
+import { Pair } from '../pair';
+@Component({
+  selector: 'app-matching-game',
+  templateUrl: './matching-game.component.html',
+  styleUrls: ['./matching-game.component.css']
+})
+
+export class MatchingGameComponent implements OnInit {
+  @Input() pairs: Pair[];
+  private unsolvedPairs: Pair[] = [];
+
+  constructor() {}
+
+  ngOnInit() {
+    for (let i = 0; i < this.pairs.length; i++) {
+      this.unsolvedPairs.push(this.pairs[i]);
+    }
+  }
+}
