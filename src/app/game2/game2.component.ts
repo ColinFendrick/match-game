c5367b3b3674429a6cd6b9ce44fee9277711d77a
Colin Fendrick <colin.fendrick@gmail.com>
2020-02-25 15:35:03 -0500

    :sparkles: Use different templates

--- src/app/game2/game2.component.ts
+++ src/app/game2/game2.component.ts
@@ -1,15 +1,13 @@
 import { Component, OnInit } from '@angular/core';
-
+import { ANIMALS } from '../animals';
 @Component({
   selector: 'app-game2',
   templateUrl: './game2.component.html',
   styleUrls: ['./game2.component.css']
 })
 export class Game2Component implements OnInit {
-
+  public animals = ANIMALS;
   constructor() { }
-
   ngOnInit() {
   }
-
 }
--- src/app/matching-game/matching-game.component.ts
+++ src/app/matching-game/matching-game.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit, Input } from '@angular/core';
+import { ContentChild, TemplateRef } from '@angular/core';
+
 import { Pair } from '../pair';
 @Component({
   selector: 'app-matching-game',
@@ -10,6 +12,9 @@ export class MatchingGameComponent implements OnInit {
   @Input() pairs: Pair[];
   private solvedPairs: Pair[] = [];
   private unsolvedPairs: Pair[] = [];
+
+  @ContentChild('leftpart', { static: false }) leftpart_temp: TemplateRef<any>;
+  @ContentChild('rightpart', { static: false }) rightpart_temp: TemplateRef<any>;
 
   constructor() {}
 
